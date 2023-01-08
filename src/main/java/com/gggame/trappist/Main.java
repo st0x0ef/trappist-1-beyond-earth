1339ece1b3795e0e71c09f0a204df28721d3954f
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2023-01-08 15:08:50 -0500

    initial commit

---
+++ src/main/java/com/gggame/trappist/Main.java
@@ -0,0 +1,34 @@
+package com.gggame.trappist;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Main.MODID)
+public class Main
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MODID = "trappist";
+
+    public Main()
+    {
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+}
